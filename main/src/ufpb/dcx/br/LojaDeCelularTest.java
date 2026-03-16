a61b37c6b0d8259bccee8b6bf31a6f362f61bebc
Mcintia816 <cintia.alcari@dcx.ufpb.br>
2026-03-16 16:00:33 -0300

    Update LojaDeCelularTest.java

--- main/src/ufpb/dcx/br/LojaDeCelularTest.java
+++ main/src/ufpb/dcx/br/LojaDeCelularTest.java
@@ -1,40 +1,45 @@
-package ufpb.dcx.br;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package LojaDeCelulares;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class LojaDeCelularTest {
-
     @Test
     public void testaCadastroPesquisaRemocao() {
-
         LojaDeCelular loja = new LojaDeCelular();
 
         try {
             loja.pesquisaCelular("123");
-            fail("Deveria lançar exceção");
-        } catch (CelularInexistenteException e) {
+            Assert.fail("Deveria lançar exceção");
+        } catch (CelularInexistenteException var6) {
         }
 
-        loja.cadastrarCelular("123", "Samsung", "S23", 4000.0);
-        loja.cadastrarCelular("456", "Apple", "iPhone 14", 7000.0);
+        loja.cadastrarCelular("123", "Samsung", "S23", (double)4000.0F, 2);
+        loja.cadastrarCelular("456", "Apple", "iPhone 14", (double)7000.0F, 2);
 
         try {
             Celular c = loja.pesquisaCelular("123");
-            assertEquals("S23", c.getModelo());
-        } catch (CelularInexistenteException e) {
-            fail("Não deveria lançar exceção");
+            Assert.assertEquals("S23", c.getModelo());
+        } catch (CelularInexistenteException var5) {
+            Assert.fail("Não deveria lançar exceção");
         }
 
         try {
             loja.removerCelular("456");
-        } catch (CelularInexistenteException e) {
-            fail("Não deveria lançar exceção");
+        } catch (CelularInexistenteException var4) {
+            Assert.fail("Não deveria lançar exceção");
         }
 
         try {
             loja.pesquisaCelular("456");
-            fail("Deveria lançar exceção");
-        } catch (CelularInexistenteException e) {
+            Assert.fail("Deveria lançar exceção");
+        } catch (CelularInexistenteException var3) {
         }
+
     }
 }
