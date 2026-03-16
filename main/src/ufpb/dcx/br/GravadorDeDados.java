fa19a5357ad2553d28b506a0f419650f3b9cc4a5
Mcintia816 <cintia.alcari@dcx.ufpb.br>
2026-03-16 15:59:27 -0300

    Refactor GravadorDeDados to use consistent file path

--- main/src/ufpb/dcx/br/GravadorDeDados.java
+++ main/src/ufpb/dcx/br/GravadorDeDados.java
@@ -1,31 +1,27 @@
-package ufpb.dcx.br;
-import java.io.*;
+
+
+package LojaDeCelulares;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GravadorDeDados {
-
     private static final String ARQUIVO = "loja_celulares.dat";
 
     public void gravarDados(Map<String, Celular> dados) throws IOException {
-        ObjectOutputStream oos = new ObjectOutputStream(
-                new FileOutputStream(ARQUIVO)
-        );
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("loja_celulares.dat"));
         oos.writeObject(dados);
         oos.close();
     }
 
-    @SuppressWarnings("unchecked")
-    public HashMap<String, Celular> recuperarDados()
-            throws IOException, ClassNotFoundException {
-
-        ObjectInputStream ois = new ObjectInputStream(
-                new FileInputStream(ARQUIVO)
-        );
-
-        HashMap<String, Celular> dados =
-                (HashMap<String, Celular>) ois.readObject();
-
+    public HashMap<String, Celular> recuperarDados() throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("loja_celulares.dat"));
+        HashMap<String, Celular> dados = (HashMap)ois.readObject();
         ois.close();
         return dados;
     }
