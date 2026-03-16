9524363206eb385876967aeee9de6b738771a3d7
Mcintia816 <cintia.alcari@dcx.ufpb.br>
2026-03-16 16:01:30 -0300

    Refactor LojaGerenciador interface methods

--- main/src/ufpb/dcx/br/LojaGerenciador.java
+++ main/src/ufpb/dcx/br/LojaGerenciador.java
@@ -1,13 +1,24 @@
-package ufpb.dcx.br;
+
+
+package LojaDeCelulares;
+
 import java.io.IOException;
 import java.util.List;
 
 public interface LojaGerenciador {
-    public boolean cadastrarCelular(String codigo, String marca, String modelo, double preco);
-    public Celular pesquisaCelular(String codigo) throws CelularInexistenteException;
-    public void removerCelular(String codigo) throws  CelularInexistenteException;
-    public void salvarDados() throws IOException;
-    public void recuperarDados() throws IOException, ClassNotFoundException;
+    boolean cadastrarCelular(String var1, String var2, String var3, double var4, int var6);
 
+    Celular pesquisaCelular(String var1) throws CelularInexistenteException;
 
+    void removerCelular(String var1) throws CelularInexistenteException;
+
+    void salvarDados() throws IOException;
+
+    void recuperarDados() throws IOException, ClassNotFoundException;
+
+    List<Celular> pesquisarCelularPeloModelo(String var1);
+
+    List<Celular> pesquisarCelularPelaMarca(String var1) throws CelularInexistenteException;
+
+    double calcularTotalDaVendaDoCelular(String var1);
 }
